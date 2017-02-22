30885a933369679e9e6d5ddc7c1f962156159882
benjaminapetersen <admin@benjaminapetersen.me>
2017-02-22 11:07:35 -0500

    Further prototype

--- app/scripts/directives/notifications/notificationDrawerWrapper.js
+++ app/scripts/directives/notifications/notificationDrawerWrapper.js
@@ -13,6 +13,11 @@ angular
         '$interval',
         'notifications',
         function($scope, $interval, notifications) {
+          var actions = [{
+            name: 'Action 1',
+            title: 'Action 1 title'
+          }];
+
           angular.extend($scope, {
             drawerHidden: true,
             allowExpand: true,
@@ -22,38 +27,52 @@ angular
               $scope.hideDrawer = !$scope.hideDrawer;
             },
             notificationGroups: [{
-              heading: 'Tab 1',
-              subHeading: 'woohoo!',
+              // TODO: link to the resource page if possible?
+              heading: 'mongodb',
+              subHeading: 'Deployment Config',
               notifications: [{
                 unread: true,
-                message: 'Hello world',
+                // TODO: this is text straight back from the event itself.
+                // should we reformat it?  I imagine we have to deal with it as it is...
+                message: 'Created new replication controller \"mongodb-2\" for version 2',
                 status: 'info',
                 timeStamp: Date.now(),
-                actions: [{
-                  name: 'Foo',
-                  title: 'Bar'
-                }, {
-                  message: 'Hello again'
-                }, {
-                  message: 'Lots of hello.'
-                }]
+                actions: actions
+              }, {
+                message: 'Created new replication controller \"mongodb-3\" for version 3',
+                status: 'info',
+                unread: true
+              }, {
+                message: 'Created new replication controller \"mongodb-4\" for version 4',
+                status: 'info',
+                unread: false
               }]
             }],
             actionButtonTitle: 'Mark All Read',
             actionButtonCallback: function() {
               console.log('drawer.actionCallback() - Mark All Read');
             },
-            // TODO:
-            // retry with {{headingInclude}}
-            // and then comment back on
-            // https://github.com/patternfly/angular-patternfly/issues/419#issuecomment-280478557
             headingInclude: 'views/directives/notifications/heading.html',
             subheadingInclude: 'views/directives/notifications/subheading.html',
             notificationBodyInclude: 'views/directives/notifications/notification-body.html',
             notificationFooterInclude: 'views/directives/notifications/notification-footer.html',
-            // TODO: I believe this is for a single group, not a single notification
+            // essentially functions to pass to the notification-body
             customScope: {
-              clearAll: function() {}
+              clearAll: function() { console.log('Clear all'); },
+              markRead: function(notification) { console.log('Mark read'); notification.unread = false; },
+              handleAction: function() { console.log('Handle action'); },
+              getNotficationStatusIconClass: function(notification) {
+                switch (notification.status ) {
+                  case 'info' :
+                    return 'pficon pficon-info';
+                  case 'error':
+                    return 'pficon pficon-error-circle-o';
+                  case 'warning':
+                    return 'pficon pficon-warning-triangle-o';
+                  case 'ok':
+                    return 'pficon pficon-ok';
+                }
+              }
             }
           });
 
@@ -64,9 +83,8 @@ angular
             });
           });
 
-          // TODO: need to add to the notificationGroups... or
-          // perhaps use a service as a cache to ensure isn't destroyed on
-          // page load/renders?  should be a single source of truth for data.
+          // TODO: Do we need a service behind this to cache the events?
+          // it looks like events fire frequently & maintain some history naturally...
           // notifications.subscribe('notification:new', function(data) {
           //   $scope.$applyAsync(function() {
           //     console.log('drawer data:', data);
